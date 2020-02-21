c655e3993eb8f505b83c25bb1153da1817262b56
sivomke <ocean1641@gmail.com>
2020-02-21 17:46:34 +0200

    implemented Sobel operator

--- sobel.py
+++ sobel.py
@@ -1,21 +1,13 @@
 from PIL import Image
 import numpy as np
-image = Image.open('puppy.jpg')
-# image.show()
-image_arr = np.array(image)
-height = image_arr.shape[0]
-width = image_arr.shape[1]
-# red, green, blue channels
-r, g, b = image_arr[:, :, 0], image_arr[:, :, 1], image_arr[:, :, 2]
-grey = np.zeros([height, width, 3], dtype = np.uint8)
-grey[:, :, 0] = 0.299*r + 0.587*g + 0.114*b
-grey[:, :, 1] = grey[:, :, 0]
-grey[:, :, 2] = grey[:, :, 0]
-Image.fromarray(grey).show()
-
 
 
 def rgb_to_greyscale(image: np.ndarray):
+    """
+    converts RGB image to greyscale
+    :param image: RGB image as np.array
+    :return: image with all 3 channels set to greyscale value
+    """
     height, width = image.shape[0], image.shape[1]
     grey = np.zeros(shape=[height, width, 3], dtype = np.uint8)
     grey[:, :, 0] = 0.2989*image[:, :, 0] + 0.5870*image[:, :, 1] + 0.1140 * image[:, :, 2]
@@ -26,9 +18,10 @@ def rgb_to_greyscale(image: np.ndarray):
 
 def sobel(image: np.ndarray) -> np.ndarray:
     """
+    applies sobel operator to color intensities
     coords: x: rows, y: cols
     :param image:
-    :return:
+    :return: result as array
     """
     width = image.shape[1]
     height = image.shape[0]
@@ -48,15 +41,24 @@ def sobel(image: np.ndarray) -> np.ndarray:
             result[x, y] = grad
     return result
 
-greysc = rgb_to_greyscale(image_arr)
-Image.fromarray(greysc, 'RGB').show()
-sobel_result = np.zeros(shape=[height, width, 3], dtype=np.uint8)
-sobel_result[:, :, 0] = sobel(image_arr[:, :, 0])
-sobel_result[:, :, 1] = sobel_result[:, :, 0]
-sobel_result[:, :, 2] = sobel_result[:, :, 0]
-result = Image.fromarray(sobel_result, 'RGB')
-result.save('sobel.png')
-result.show()
+
+def main():
+    image = Image.open('puppy.jpg')
+    # image.show()
+    image_arr = np.array(image)
+    height = image_arr.shape[0]
+    width = image_arr.shape[1]
+    # red, green, blue channels
+    r, g, b = image_arr[:, :, 0], image_arr[:, :, 1], image_arr[:, :, 2]
+    greysc = rgb_to_greyscale(image_arr)
+
+    sobel_result = np.zeros(shape=[height, width, 3], dtype=np.uint8)
+    sobel_result[:, :, 0] = sobel(image_arr[:, :, 0])
+    sobel_result[:, :, 1] = sobel_result[:, :, 0]
+    sobel_result[:, :, 2] = sobel_result[:, :, 0]
+    result = Image.fromarray(sobel_result, 'RGB')
+    result.save('sobel.png')
+    result.show()
 
 
 
